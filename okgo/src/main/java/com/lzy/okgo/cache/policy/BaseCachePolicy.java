444ee7584742912e9f58b9fe6fcbe4a0d5757d7b
kelen <3154744507@qq.com>
2020-10-15 10:52:01 +0800

    refactor:更新okhttp版本并支持callTimeout设置

--- okgo/src/main/java/com/lzy/okgo/cache/policy/BaseCachePolicy.java
+++ okgo/src/main/java/com/lzy/okgo/cache/policy/BaseCachePolicy.java
@@ -29,6 +29,7 @@ import com.lzy.okgo.utils.HeaderParser;
 import com.lzy.okgo.utils.HttpUtils;
 
 import java.io.IOException;
+import java.io.InterruptedIOException;
 import java.net.SocketTimeoutException;
 
 import okhttp3.Call;
@@ -142,6 +143,12 @@ public abstract class BaseCachePolicy<T> implements CachePolicy<T> {
                     if (!call.isCanceled()) {
                         Response<T> error = Response.error(false, call, null, e);
                         onError(error);
+                    } else {
+                        // 这个else是为了解决 callTimeout 无法触发 onError 回调
+                        if (e instanceof InterruptedIOException) {
+                            Response<T> error = Response.error(false, call, null, e);
+                            onError(error);
+                        }
                     }
                 }
             }
